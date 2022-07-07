3da2937321229340e6a02f69528f3ea6f3e06e6b
Malik <malikspruill@gmail.com>
2022-07-07 19:01:10 -0400

    Add initial sub-directories and files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button variant="contained">Hello World</Button>
+    </>
+  )
+}
+
+export default App;
